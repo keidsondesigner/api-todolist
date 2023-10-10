805b9d17f94d434ed69cc00029a02c97816d30fb
Keidson Roby <keidsondesigner@gmail.com>
2023-10-09 23:09:51 -0400

    feat: :tada: initial commit

---
+++ src/test/java/br/com/keidsonroby/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.keidsonroby.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
